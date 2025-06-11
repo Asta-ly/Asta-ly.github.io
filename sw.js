75db910231ef97a9f9e78ef1c8d9d27049256224
Asta-ly <2115084902@qq.com>
2025-06-11 16:58:20 +0800

    Site updated: 2025-06-11 16:58:19

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/16107/',
+'/','/posts/29421/','/posts/16107/',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
